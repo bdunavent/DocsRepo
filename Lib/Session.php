9a7926ae7c47da3ce9fab6382934c1c6708d2e6d
bdunavent <b.dunavent@gmail.com>
2014-03-31 21:49:26 -0400

    Session Timeout
    - Updated the Users::validateUser() function to set a timeout value of
    the current time upon successful user authentication
    - Updated Session::validateSession() function to check that the timeout
    window has not been reached or exceeded; if so, redirect to the login
    page and destroy the session, otherwise, just update the current timeout
    timestamp to the current time

--- Lib/Session.php
+++ Lib/Session.php
@@ -7,9 +7,27 @@ Class Session
   public static function validateSession()
   {
     session_start();
+
+    // check to see if a user session is registered, else redirect to login
     if (!isset($_SESSION['name']) && !isset($_SESSION['email']) && !isset($_SESSION['userType']))
     {
       header("location:../Authentication/login.php");
+    }
+    else
+    {
+      // if user session is registered, check to see if the 15 min timeout window
+      // has elapsed
+      if ($_SESSION['timeout'] + 15 * 60 < time())
+      {
+        // if so, log out/destroy the session and redirect to login
+        $this -> logOutSession();
+        header("location:../Authentication/login.php");
+      }
+      else
+      {
+        // if not, update the timeout to a recent timestamp
+        $_SESSION['timeout'] = time();
+      }
     }
   }
 
@@ -36,5 +54,6 @@ Class Session
     session_start();
     return session_destroy();
   }
+
 }
 ?>
